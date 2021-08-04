6705aaa0657750f2add3123c686e4ee2852c4362
sammyhy <sammykkoon@gmail.com>
2021-08-04 16:37:06 +0900

    learn_express

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("모든 요청 전부 실행");
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("get / 요청만 실행");
+    next();
+  },
+  (req, res) => {
+    //   res.send("Hello, World~~");
+    //res.sendFile(path.join(__dirname, "/index.html"));
+    throw new Error("에러는 에러 처리 미들웨어로~");
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
